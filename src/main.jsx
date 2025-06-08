fae8efe18645fffc451df71e65ad1ed2dc30b868
Cristian Aguirre <107177715+caguirre5@users.noreply.github.com>
2025-06-08 17:53:11 -0600

    Done

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom'; // Importar el enrutador
+import { HashRouter as Router } from 'react-router-dom'; // Importar el enrutador
 import './index.css';
 import App from './App.jsx';
 

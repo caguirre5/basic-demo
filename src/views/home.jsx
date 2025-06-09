8f6962a612b0b263d23956fc4b51d6603b06a096
Cristian Aguirre <107177715+caguirre5@users.noreply.github.com>
2025-06-08 18:03:50 -0600

    Done

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -110,10 +110,10 @@ const Home = () => {
 
                 <main ref={servicesRef} className="min-h-screen px-4 py-8 bg-[#DEDBD6] flex flex-col items-center justify-center text-center">
                     <h2 className="text-2xl lg:text-3xl font-avenir-book mt-4 lg:mb-6 text-[#A76734]">OUR SERVICES</h2>
-                    <div className="flex justify-center items-center flex-col lg:flex-row space-x-16 w-full">
+                    <div className="flex justify-center items-center flex-col lg:flex-row lg:space-x-16 w-full">
                         {/* Autonomy Section */}
-                        <div className="lg:w-[16%] py-10 flex flex-col justify-center">
-                            <img src={M4_autonomyImage} alt="Autonomy" className=" h-[50vh]" />
+                        <div className="lg:w-[16%] py-10 flex flex-col justify-center items-center">
+                            <img src={M4_autonomyImage} alt="Autonomy" className=" lg:h-[50vh]" />
                             <h3 className="text-2xl font-lustria mt-4 text-[#215A81]">Coffee Roasting</h3>
                             <div className='bg-[#A76734] mx-auto h-1 rounded-full w-28 my-2'/>
                             <p className="lg:text-lg text-[#8292A7] mb-2">We roast green coffee beans into rich, flavorful profiles. Our roasting services are tailored to meet your business needs and taste preferences.</p>
@@ -123,18 +123,18 @@ const Home = () => {
                         </div>
 
                         {/* Mastery Section */}
-                        <div className="lg:w-[16%] py-10 flex flex-col justify-center items">
-                            <img src={M4_masteryImage} alt="Mastery" className=" h-[50vh]" />
-                            <h3 className="text-2xl font-lustria mt-4 text-[#215A81]">Coffee Roasting</h3>
+                        <div className="w-full lg:w-[16%] py-10 flex flex-col justify-center items-center">
+                            <img src={M4_masteryImage} alt="Mastery" className="lg:h-[50vh]" />
+                            <h3 className="text-2xl font-lustria mt-4 text-[#215A81]">Packaging & Delivery</h3>
                             <div className='bg-[#A76734] mx-auto h-1 rounded-full w-28 my-2'/>
-                            <p className="lg:text-lg text-[#8292A7] mb-2">We roast green coffee beans into rich, flavorful profiles. Our roasting services are tailored to meet your business needs and taste preferences.</p>
+                            <p className="lg:text-lg text-[#8292A7] mb-2">We provide professional packaging and reliable delivery to ensure your coffee arrives fresh, aromatic, and beautifully presented.</p>
                             {/* <button className="mx-auto mt-2 lg:mt-10 text-[#A76734] font-semibold py-2 px-4 border border-[#A76734] rounded-full shadow hover:bg-[#A76734] hover:text-[#DED8D6] focus:outline-none focus:ring-2 focus:ring-[#FFD8CC] focus:ring-opacity-50">
                                 GET TO KNOW US
                             </button> */}
                         </div>
 
                         {/* Purpose Section */}
-                        <div className="lg:w-[16%] py-10 flex flex-col justify-center">
+                        <div className="lg:w-[16%] w-full py-10 flex flex-col justify-center items-center">
                             <img src={M4_purposeImage} alt="Purpose" className=" h-[50vh]" />
                             <h3 className="text-2xl font-lustria mt-4 text-[#215A81]">Quality Control & Cupping</h3>
                             <div className='bg-[#A76734] mx-auto h-1 rounded-full w-28 my-2'/>
